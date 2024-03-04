6aabf11fd32cf1ed4507e2fb57a1fd7be79330e0
Fred Weigel <fred_weigel@hotmail.com>
2024-03-04 01:02:03 -0500

    My take on crrd

---
+++ test.c
@@ -0,0 +1,113 @@
+/*
+ * Test functions for crrd
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "crrd.h"
+
+/*
+ * Two macros:
+ *
+ * SEC2HR converts time in seconds to hrtime_t
+ * HR2SEC converts hrtime_t to seconds
+ */
+#define SEC2HR(s) ((hrtime_t)((s) * 1000LL * 1000LL * 1000LL))
+#define HR2SEC(s) ((long)((s) / (1000LL * 1000LL * 1000LL)))
+
+void
+rrd_print(rrd_t *r)
+{
+
+	for (size_t i = 0; i < rrd_len(r); i++) {
+		const rrd_data_t *data;
+
+		data = rrd_entry(r, i);
+		printf("%lld: %ld\n", data->rrdd_time, data->rrdd_txg);
+	}
+}
+
+void
+dbrrd_print(dbrrd_t *db)
+{
+
+	printf("Minutes list:\n");
+	rrd_print(&db->dbr_minutes);
+	printf("Days list:\n");
+	rrd_print(&db->dbr_days);
+	printf("Months list:\n");
+	rrd_print(&db->dbr_months);
+}
+
+#define CCRD_ASSERT_EQ(db, time, txg) do { 					\
+		if (dbrrd_query(db, time) != txg) {                     	\
+			printf("%d failed: query(%lld) %d != %d\n",		\
+			    __LINE__, time, dbrrd_query(db, time), txg);	\
+			dbrrd_print(db);					\
+			exit(1);						\
+		}								\
+	} while (0)
+
+
+void
+lineral_test()
+{
+	struct {
+		hrtime_t time;
+		uint64_t txg;
+	} test[] = {
+		{5, 1},
+		{10, 2},
+		{15, 3},
+		{20, 4},
+		{30, 5},
+	};
+	dbrrd_t db = {0};
+
+	for (size_t i = 0; i < sizeof(test) / sizeof(test[0]); i++) {
+		dbrrd_add(&db, test[i].time, test[i].txg);
+		CCRD_ASSERT_EQ(&db, test[i].time, test[i].txg);
+	}
+
+	for (size_t i = 0; i < sizeof(test) / sizeof(test[0]); i++) {
+		CCRD_ASSERT_EQ(&db, test[i].time, test[i].txg);
+	}
+}
+
+void
+days_test()
+{
+	dbrrd_t db = {0};
+
+	for (size_t i = 0; i < 1024; i++) {
+		dbrrd_add(&db, SEC2HR(5 * i * 60 + 60 * 3600), i);
+	}
+
+	CCRD_ASSERT_EQ(&db, SEC2HR(5 * 60 + 60 * 3600), 0);
+}
+
+void
+month_test()
+{
+	dbrrd_t db = {0};
+
+	for (size_t i = 0; i < 1024; i++) {
+		dbrrd_add(&db, SEC2HR(i * 60 * 3660 + 60 * 3600 * 30), i);
+	}
+
+	CCRD_ASSERT_EQ(&db, SEC2HR(2 * 60 * 3600 * 30), 30);
+}
+
+int
+main(int ac, char **av)
+{
+	printf("crrd - C RRD Database\n");
+	lineral_test();
+	days_test();
+	month_test();
+	printf("test passed");
+
+	return (0);
+}
